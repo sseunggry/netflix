389c30bebd1bc78b669fa6560f12f84ae27c81fd
sseunggry <sseunggry@gmail.com>
2023-01-24 16:15:28 +0900

    head 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Helmet} from "react-helmet";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
@@ -7,6 +8,9 @@ import Header from "./Components/Header";
 function App() {
     return (
         <Router>
+            <Helmet>
+                <title>netflix</title>
+            </Helmet>
             <Header />
             <Switch>
                 <Route path={["/tv", "/tv/:tvId"]}>
